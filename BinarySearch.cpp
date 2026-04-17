f5c9a2782c5eaf246bc5c510b775d56226dfb719
Martzz-a <maritzafidelyah@gmail.com>
2026-04-17 15:03:53 +0700

    Menambahkan Do While Loop untuk Mengurutkan secara Bubble Sort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -36,4 +36,17 @@ void input()
 void bubbleSortArray()
 {
     int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang; j++)
+        {
+            if (element[j] > element[j+1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
 }
